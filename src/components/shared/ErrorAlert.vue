a663e54c786a18d66383674b7ead60c8a4ec626d
souzaWill <contato.willsouza@gmail.com>
2025-07-19 23:06:30 -0300

    add loading and erro component

---
+++ src/components/shared/ErrorAlert.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-alert :text="message" v-if="message" type="error" class="mb-4" />
+</template>
+
+<script setup lang="ts">
+const { message } = defineProps<{
+  message: string | null
+}>()
+</script>
