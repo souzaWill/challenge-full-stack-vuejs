d12a32f0ae00ef82c8183daf336478521a5fc6f2
souzaWill <contato.willsouza@gmail.com>
2025-07-20 12:34:44 -0300

    feat: logout and navigation drawer

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -1,15 +1,18 @@
 <template>
   <v-app>
-    <v-app-bar color="primary" app>
-      <template v-slot:prepend>
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      </template>
-
-      <v-toolbar-title>Alunos</v-toolbar-title>
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-nav-icon @click="toggle"></v-app-bar-nav-icon>
+      <v-toolbar-title>{{ 'Gereciamento de Matriculas +A Educacao' }}</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text @click="logout"> Logout </v-btn>
     </v-app-bar>
 
-    <v-navigation-drawer app>
-      <!-- Menu lateral -->
+    <v-navigation-drawer app v-model="drawer" color="primary lighten-4">
+      <v-list>
+        <v-list-item link @click="navigate('/')">
+          <v-list-item-title>{{ 'Alunos' }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
     <v-main>
@@ -19,3 +22,26 @@
     </v-main>
   </v-app>
 </template>
+
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/auth'
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const drawer = ref(true)
+const authStore = useAuthStore()
+
+const logout = async () => {
+  authStore.logout()
+  router.push({ path: '/login' })
+}
+
+const toggle = () => {
+  drawer.value = !drawer.value
+}
+
+const navigate = (route: string) => {
+  router.push(route)
+}
+</script>
