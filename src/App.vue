9781634d8948dd814972de40782934ef07461544
souzaWill <contato.willsouza@gmail.com>
2025-07-23 20:04:56 -0300

    feat: using store to put app in loading state

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import AppLoading from './components/shared/AppLoading.vue'
+import { useLoadingStore } from './stores/loading'
+
+const loadingStore = useLoadingStore()
 </script>
 
 <template>
+  <AppLoading :show="loadingStore.loading" />
   <RouterView />
 </template>
