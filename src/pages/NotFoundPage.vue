80874449a90804dea2b95c05beff48c50650f543
souzaWill <contato.willsouza@gmail.com>
2025-07-23 20:05:17 -0300

    feeat: add 404 page

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container class="fill-height d-flex flex-column justify-center align-center text-center">
+    <v-icon size="80" color="error">mdi-alert-circle-outline</v-icon>
+    <h1 class="text-h2 mt-4">404</h1>
+    <p class="text-subtitle-1 mb-6">Página não encontrada</p>
+
+    <v-btn color="primary" variant="flat" @click="goHome"> Voltar para a página inicial </v-btn>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const goHome = () => {
+  router.push('/students')
+}
+</script>
