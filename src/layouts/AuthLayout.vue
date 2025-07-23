29ef11a902e471d4696f1757544d6ae886c07e07
souzaWill <contato.willsouza@gmail.com>
2025-07-23 12:41:48 -0300

    feat: define default drawer state to true

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -29,7 +29,7 @@ import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
-const drawer = ref(false)
+const drawer = ref(true)
 const authStore = useAuthStore()
 
 const logout = async () => {
