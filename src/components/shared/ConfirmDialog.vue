196fde987ef988eb4e892f396a31e48a3c03c039
souzaWill <contato.willsouza@gmail.com>
2025-07-24 13:29:24 -0300

    feat: final adjusts to confirmDialog component

--- src/components/shared/ConfirmDialog.vue
+++ src/components/shared/ConfirmDialog.vue
@@ -1,13 +1,13 @@
 <template>
-  <v-dialog :model-value="show" max-width="500px">
+  <v-dialog :model-value="props.show" max-width="500px">
     <v-card>
-      <v-card-title class="text-h5">{{ title }}</v-card-title>
-      <v-card-text>{{ text }}</v-card-text>
+      <v-card-title class="text-h5">{{ props.title }}</v-card-title>
+      <v-card-text>{{ props.text }}</v-card-text>
       <v-divider />
       <v-card-actions>
         <v-spacer />
-        <v-btn color="blue darken-1" text @click="emit('cancel')">{{ 'Cancel' }}</v-btn>
-        <v-btn color="blue darken-1" text @click="emit('confirm')">{{ 'Confirm' }}</v-btn>
+        <v-btn color="blue darken-1" text @click="emit('cancel')">Cancelar</v-btn>
+        <v-btn color="red darken-1" text @click="emit('confirm')">Confirmar</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
