03fd1af8b6f5d4ceb6b5209ed006d1231b2d9411
souzaWill <contato.willsouza@gmail.com>
2025-07-24 17:27:40 -0300

    feat: define icon to red

--- src/components/shared/TableRowActions.vue
+++ src/components/shared/TableRowActions.vue
@@ -1,6 +1,6 @@
 <template>
   <v-icon class="me-2" size="small" @click="emit('edit', item)"> mdi-pencil </v-icon>
-  <v-icon size="small" @click="emit('delete', item)"> mdi-delete </v-icon>
+  <v-icon size="small" color="red" @click="emit('delete', item)"> mdi-delete </v-icon>
 </template>
 
 <script setup lang="ts">
